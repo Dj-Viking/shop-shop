b77123767fb7817bc514e969f8f107ca4944a9f3
DJ Viking <anders.swedishviking@gmail.com>
2020-10-25 15:21:44 -0400

    able to change global state of the current category on front end

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,8 +1,16 @@
 import gql from 'graphql-tag';
 
 export const QUERY_PRODUCTS = gql`
-  query getProducts($category: ID) {
-    products(category: $category) {
+  query getProducts
+  (
+    $category: ID
+  ) 
+  {
+    products
+    (
+      category: $category
+    ) 
+    {
       _id
       name
       description
