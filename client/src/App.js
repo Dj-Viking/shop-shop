72c5b4a4335e5aa0d122f7c815d3481fc2efb481
DJ Viking <anders.swedishviking@gmail.com>
2020-10-25 14:41:58 -0400

    22.1.5 finishing up the globalstate.js file

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
 
+import { StoreProvider } from './utils/GlobalState.js';
+
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
---
+++ client/src/utils/GlobalState.js
@@ -0,0 +1,45 @@
+import React, { createContext, useContext } from 'react';
+import { useProductReducer } from './reducers';
+const StoreContext = createContext();
+const { Provider } = StoreContext;
+
+//StoreContext is just an empty container waiting for data to be provided
+// to it as a state. now we create a custom provider function
+// that will be used to manage and update our state using the reducer
+// created earlier.
+// having this all bundled up will make it easier to integrate
+// into the app
+
+//with this function we instantiate the initial global state with the
+// useProductReducer() custom function so we can wrap it with
+// the useReducer() hook from React
+// now every time we run this function we receive two items
+// * state - most up to date version of the global state object
+// * dispatch - a method executed to update the state. it is
+// specifically goin to look for an action object passed in
+// as its argument
+
+//after useProductReducer() completes and provides with a new state
+// and function to update the state. return the StoreContext's <Provider />
+// component with the state object and dispatch the function provided
+// as data for the `value` prop
+
+const StoreProvider = ({ value = [], ...props}) => {
+  const [state, dispatch] = useProductReducer
+  (
+    {
+      products: [],
+      categories: [],
+      currentCategory: '',
+    }
+  );
+  //use this to confirm it works
+  console.log(state);
+  return <Provider value={[state, dispatch]} {...props}/>
+};  
+
+const useStoreContext = () => {
+  return useContext(StoreContext);
+};
+
+export { StoreProvider, useStoreContext };
